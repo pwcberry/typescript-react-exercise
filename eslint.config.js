0c179dccfc50322adb7d7ce0d385b8e4170608d3
Peter Berry <peter-berry@rea-group.com>
2025-01-27 14:13:45 +1100

    Add linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -5,10 +5,16 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["*.{ts,tsx}", "components/*.{ts,tsx}", "utils/*.{ts,tsx}"],},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat['jsx-runtime'],
+    {files: ["*.{ts,tsx}", "components/*.{ts,tsx}", "utils/*.{ts,tsx}"]},
+    {languageOptions: {globals: globals.browser}},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    pluginReact.configs.flat['jsx-runtime'],
+    {
+        rules: {
+            semi: ["error", "always"],
+            "comma-dangle": ["error", "only-multiline"],
+        },
+    },
 ];
